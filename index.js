f169e9b06d91e69e7b89e037a567141735ead932
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-11-25 17:47:05 +0600

    'categories-added'

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('colors')
+require('dotenv').config()
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
+const uri = process.env.URI
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
-    
+    try{
+        const categoriesCollection = client.db('rc-cars').collection('categories')
+
+        app.get('/categories', async(req, res) => {
+            const query = {}
+            const options = await categoriesCollection.find(query).toArray()
+            res.send(options)
+        })
+
+        // app.get('/categories/:name', async (req, res) => {
+        //     const name = req.params.name
+        // })
+
+
+    }
+    finally{
+
+    }
+
+
 }
 
 run().catch(console.log)
