f9b9dba8cb864e4a9a7ad528519d665af6ecd765
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-11-27 19:11:36 +0600

    'add-user-api-added'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('colors')
 require('dotenv').config()
@@ -14,12 +14,13 @@ const uri = process.env.URI
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try{
+    try {
         const categoriesCollection = client.db('rc-cars').collection('categories')
         const productsCollection = client.db('rc-cars').collection('products')
         const bookingsCollection = client.db('rc-cars').collection('bookings')
+        const usersCollection = client.db('rc-cars').collection('users')
 
-        app.get('/categories', async(req, res) => {
+        app.get('/categories', async (req, res) => {
             const query = {}
             const options = await categoriesCollection.find(query).toArray()
             res.send(options)
@@ -27,7 +28,7 @@ async function run() {
 
         app.get('/categories/:name', async (req, res) => {
             const name = req.params.name
-            const query = { category : name }
+            const query = { category: name }
             const options = await productsCollection.find(query).toArray()
             // console.log(options)
             res.send(options)
@@ -41,20 +42,28 @@ async function run() {
 
         app.get('/bookings', async (req, res) => {
             const email = req.query.email
-            const query = { email : email}
+            const query = { email: email }
             const myBookings = await bookingsCollection.find(query).toArray()
             // console.log(myBookings)
             res.send(myBookings)
         })
 
-        app.post('/addproduct', async(req, res) => {
+        app.post('/addproduct', async (req, res) => {
             const data = req.body;
             const result = await productsCollection.insertOne(data)
             res.send(result)
         })
 
+        app.post('/adduser', async (req, res) => {
+            const data = req.body
+            const result = await usersCollection.insertOne(data)
+            res.send(result)
+        })
+
+        
+
     }
-    finally{
+    finally {
 
     }
 
